a2f01095f87216e6f23f20408af517971549fd20
@64LONG <64298723+64LONG@users.noreply.github.com>
2025-11-04 22:25:40 -0600

    Testing functionality by providing return value

--- 01_helloWorld/helloWorld.js
+++ 01_helloWorld/helloWorld.js
@@ -1,5 +1,12 @@
 const helloWorld = function() {
-  return ''
+  //Testing function with example return value
+  //Results in FAILURE due to .spec.js not seeing the defined value
+  //return 'toast'
+  //Testing function with expected return value
+  //return 'Hello, World!'
+  //Testing function with example return value
+  return 'Hello, World!'
+
 };
 
 module.exports = helloWorld;
